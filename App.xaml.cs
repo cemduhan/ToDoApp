773e7969ef736ad103c5e2540b871667a634f19a
D. Cem Karagöz <dhncem@yandex.com>
2019-10-11 20:55:54 +0300

    added app conf

---
+++ App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+using ToDoApp.Database.Repository;
+using ToDoApp.Model;
+using ToDoApp.Service;
+
+namespace ToDoApp
+{
+    /// <summary>
+    /// App.xaml etkileşim mantığı
+    /// </summary>
+    public partial class App : Application
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IToDoListItemRepository, ToDoListItemRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IToDoListRepository, ToDoListRepository>();
+        }
+    }
+}
